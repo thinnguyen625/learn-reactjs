5db166163a9b259095fa4e1b6db4d56963392137
EricNguyen99 <vanthin7111999@gmail.com>
2020-02-07 23:38:17 +0700

    completed handle add form style

--- lesson08-form/src/App.js
+++ lesson08-form/src/App.js
@@ -1,34 +1,35 @@
 import React, { Component } from 'react';
 import './App.css';
 
-
-
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       txtUsername: '',
-      txtPassword: ''
+      txtPassword: '',
+      txtDesc: 'Sản phẩm này rất tốt',
+      sltGender: 0,
+      rdLang: "vi",
+      chkbStatus: "false"
     };
   }
 
   onHandleChange = (event) => {
     var target = event.target;
-    var name = event.name;
-    var value = event.value;
+    var name = target.name;
+    var value = target.type === 'checkbox' ? target.checked : target.value;
     this.setState({
       [name]: value
     })
   }
 
   onHandleSubmit = (event) => {
-    event.prevenDefault();
+    event.preventDefault();
     console.log(this.state);
-
   }
+
   render() {
     return (
-
       <div className="container mt-30">
         <div className="row">
           <div className="col-xs-8 col-sm-8 col-md-8 col-lg-8">
@@ -38,33 +39,95 @@ class App extends Component {
                 <h3 className="panel-title">FORM</h3>
               </div>
               <div className="panel-body">
-                
+
                 <form onSubmit={this.onHandleSubmit}>
-                  
+
                   <div className="form-group">
                     <label>Username: </label>
-                    <input 
-                    type="text" 
-                    className="form-control" 
-                    name="txtUsername"
-                    onChange={this.onHandleChange}
+                    <input
+                      type="text"
+                      className="form-control"
+                      name="txtUsername"
+                      onChange={this.onHandleChange}
+                      value={this.state.txtUsername}
                     />
                   </div>
 
                   <div className="form-group">
                     <label >Password: </label>
-                    <input 
-                    type="password" 
-                    className="form-control" 
-                    name="txtPassword"
-                    onChange={this.onHandleChange}
+                    <input
+                      type="password"
+                      className="form-control"
+                      name="txtPassword"
+                      onChange={this.onHandleChange}
+                      value={this.state.txtPassword}
                     />
+                  </div>
+
+                  <div className="form-group">
+                    <label >Mô tả: </label>
+                    <textarea
+                      className="form-control"
+                      rows="3"
+                      name="txtDesc"
+                      onChange={this.onHandleChange}
+                      value={this.state.txtDesc}
+                    ></textarea>
+                  </div>
+
+                  <label>Giới tính: </label>
+                  <select
+                    className="form-control"
+                    name="sltGender"
+                    value={this.state.sltGender}
+                    onChange={this.onHandleChange}
+                  >
+                    <option value={0}>Nam</option>
+                    <option value={1}>Nữ</option>
+
+                  </select>
+                  <br />
+                  <label>Ngôn ngữ: </label>
+                  <div className="radio">
+                    <label>
+                      <input
+                        type="radio"
+                        name="rdLang"
+                        value="vi"
+                        onChange={this.onHandleChange}
+                        checked={this.state.rdLang === "vi"}
+                      />
+                      Tiếng Việt
+                    </label>
+                    <br />
+                    <label>
+                      <input
+                        type="radio"
+                        name="rdLang"
+                        value="en"
+                        onChange={this.onHandleChange}
+                        checked={this.state.rdLang === "en"}
+                      />
+                      Tiếng Anh
+                    </label>
+                  </div>
+                  <div className="checkbox">
+                    <label>
+                      <input
+                        type="checkbox"
+                        name="chkbStatus"
+                        value={true}
+                        onChange={this.onHandleChange}
+                        checked={this.state.chkbStatus === true}
+
+                      />
+                      Trạng thái:
+                      </label>
                   </div>
 
                   <button type="submit" className="btn btn-primary">Lưu</button> &nbsp;
                   <button type="reset" className="btn btn-default">Xóa trắng</button>
                 </form>
-                
 
               </div>
             </div>
